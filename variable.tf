a71d45f2f334ee2b49a549298e2307ef0f8c6cae
ajithengg13 <84135675+ajithengg13@users.noreply.github.com>
2021-09-22 20:40:06 +0530

    1.0

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "medium"
+}
+
+variable "resource_group_location" {
+  default = "centralindia"
+}
+
+variable "cosmos_db_account_name" {
+  default = "cosmosmongodb"
+}
+
+variable "failover_location" {
+  default = "westindia"
+}
