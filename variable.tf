fc3e621946f644dc7ecc85071c22549400d02b96
ajithengg13 <84135675+ajithengg13@users.noreply.github.com>
2021-09-22 21:06:26 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,9 +2,6 @@ variable "resource_group_name" {
   default = "myproject3"
 }
 
-variable "resource_group_location" {
-  default = "centralindia"
-
 variable "cosmos_db_account_name" {
   default = "ajithenggcfs1"
 }
